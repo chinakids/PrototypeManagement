c3fee002a4ea9d37789bf211618d959c1a38c347
chinakids <494063570@qq.com>
2015-06-26 18:07:29 +0800

    new

--- routes/pr.js
+++ routes/pr.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var http = require('http');
 var router = express.Router();
 var mongoose = require('mongoose');
 var productmodel = require('../models/products');
@@ -9,11 +10,10 @@ router.get('/', function(req, res, next) {
   var id = req.query.pr;
   productmodel.findNewPr(id,function(err,product){
     if(err){
-      console.log(err)
+      //console.log(err)
       res.status(404);
     }else{
-      console.log(product)
-      res.redirect(product[0].url)
+      res.redirect(product[0].url);
     }
   })
 });
