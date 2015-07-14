484b6be700fe93300461a82296c8af6f0989f1f4
chinakids <494063570@qq.com>
2015-07-14 09:03:06 +0800

    增加了提示

--- public/js/unicorn.tables.js
+++ public/js/unicorn.tables.js
@@ -5,27 +5,27 @@
 **/
 
 $(document).ready(function(){
-	
+
 	$('.data-table').dataTable({
 		"bJQueryUI": true,
 		"sPaginationType": "full_numbers",
 		"sDom": '<""l>t<"F"fp>',
 		"aaSorting": [[0,'desc']]
 	});
-	
+
 	// var checkboxClass = 'icheckbox_flat-blue';
 	// var radioClass = 'iradio_flat-blue';
 	// $('input[type=checkbox],input[type=radio]').iCheck({
  //    	checkboxClass: checkboxClass,
  //    	radioClass: radioClass
 	// });
-	
+
 	// $('select').select2();
-	
+
 
 	$("span.icon input:checkbox, th input:checkbox").on('ifChecked || ifUnchecked',function() {
 		var checkedStatus = this.checked;
-		var checkbox = $(this).parents('.widget-box').find('tr td:first-child input:checkbox');		
+		var checkbox = $(this).parents('.widget-box').find('tr td:first-child input:checkbox');
 		checkbox.each(function() {
 			this.checked = checkedStatus;
 			if (checkedStatus == this.checked) {
@@ -184,6 +184,9 @@ $(document).ready(function(){
     uploader.options.formData.version = $('input[name=productVersion]').val(),
     uploader.options.formData.codeVersion = $('input[name=productCodeVersion]').val(),
     uploader.options.formData.status = $('input[name=productStatus]:checked').val() || 1;
+		if(!confirm('是否选择\n【'+$('select[name=productList]').find('option[value='+infoId+']').text()+'】?')){
+			return false;
+		}
     uploader.upload();
     //- var id = $('input[name=productId]').val(),
     //-     infoId = $('select[name=productList]').val(),
